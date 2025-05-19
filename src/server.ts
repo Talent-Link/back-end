5a0eb54616eb1da9438298e94e248173cf7e419d
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-05-19 14:26:35 -0300

    feat: Remove arquivo render.yaml e ajusta a porta padrão do servidor para 3000

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,19 @@ async function main() {
     await prisma.$connect();
     console.log("Conectado ao banco de dados com sucesso");
 
-    const PORT = process.env.PORT || 10000;
+    const PORT = process.env.PORT || 3000;
 
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
-      console.log(`URL de autenticação RH: http://localhost:${PORT}/auth/google/RH`);
-      console.log(`URL de autenticação Candidato: http://localhost:${PORT}/auth/google/CANDIDATO`);
+
+      console.log(
+        `URL de autenticação RH:      http://localhost:${PORT}/auth/google/RH`
+      );
+
+      console.log(
+        `URL de autenticação Candidato: http://localhost:${PORT}/auth/google/CANDIDATO`
+      );
+
       console.log(`Documentação da API: http://localhost:${PORT}/api-docs`);
     });
   } catch (error) {
