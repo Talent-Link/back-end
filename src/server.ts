bac4f2fb988fb67cd889de917200d3218c8bfe5b
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-05-19 11:03:13 -0300

    feat: Atualiza a porta padrão do servidor para 10000

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 async function main() {
   await prisma.$connect();
   
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const HOST = process.env.HOST || "0.0.0.0";
 
 
