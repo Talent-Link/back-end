5a3ef0a5bef90d91b4e4f6e3443d13927a397671
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-05-20 09:09:11 -0300

    feat: adiciona URLs de autenticação para o ambiente de produção

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,6 @@ app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
   console.log(`🔗 URL de autenticação RH: https://seu-servico-render.onrender.com/auth/google/RH`);
   console.log(`🔗 URL de autenticação Candidato: https://seu-servico-render.onrender.com/auth/google/CANDIDATO`);
+  console.log(`🔗 URL de autenticação RH: https://talentlink-wd88.onrender.com/auth/google/RH`)
+  console.log(`🔗 URL de autenticação Candidato: https://talentlink-wd88.onrender.com/auth/google/CANDIDATO`);
 });
