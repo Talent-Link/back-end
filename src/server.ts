bfe4bd97cb5a591879c04273ff07c813c2b7b0b0
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-05-20 13:53:43 -0300

    fix: adiciona nova linha na mensagem de log ao iniciar o servidor

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import app from "./app";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}\n`);
   console.log(`🔗 URL de autenticação RH: https://talentlink-wd88.onrender.com/auth/google/RH`)
   console.log(`🔗 URL de autenticação Candidato: https://talentlink-wd88.onrender.com/auth/google/CANDIDATO`);
 });
