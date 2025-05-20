46282c0d2249e6ca926953287f085ab9165f181e
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-05-20 09:13:37 -0300

    feat: atualiza URLs de autenticação no servidor e ajusta a configuração do dotenv

--- Readme.md
+++ Readme.md
@@ -3,3 +3,5 @@ npx ts-node-dev src/server.ts
 npx ts-node scripts/clearDatabase.ts
 
 npx prisma studio
+
+https://talentlink-wd88.onrender.com
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config(); // Deve ser a PRIMEIRA linha de código
+dotenv.config();
 
 import app from "./app";
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-  console.log(`🔗 URL de autenticação RH: https://seu-servico-render.onrender.com/auth/google/RH`);
-  console.log(`🔗 URL de autenticação Candidato: https://seu-servico-render.onrender.com/auth/google/CANDIDATO`);
   console.log(`🔗 URL de autenticação RH: https://talentlink-wd88.onrender.com/auth/google/RH`)
   console.log(`🔗 URL de autenticação Candidato: https://talentlink-wd88.onrender.com/auth/google/CANDIDATO`);
 });
