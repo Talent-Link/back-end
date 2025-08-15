1e46a79176cfd6ce16b7b183fec092c60695a2e2
Filipi0 <127785380+Filipi0@users.noreply.github.com>
2025-08-15 18:59:22 -0300

    feat: Atualiza URL do frontend no CORS para a nova versão do TalentLink

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ const corsOptions = {
     "http://localhost:3000",
     "https://localhost:3000", 
     "https://talentlink-wd88.onrender.com",
-    "https://talent-link-front.vercel.app",
+    "https://talent-link-five.vercel.app/",
     /\.vercel\.app$/,
     /\.onrender\.com$/
   ],
